b44a85fb97ce4a1a36abf7978737436374622916
luck22y <luckykumar2204@gmail.com>
2023-05-02 10:32:18 +0530

    nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
+  const [active, setActive] = useState('#');
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="#" className={active==='#'? 'active': ''} onClick={()=>setActive('#')}><AiOutlineHome /></a>
+      <a href="#about" className={active==='#about'? 'active': ''} onClick={()=>setActive('#about')}><AiOutlineUser /></a>
+      <a href="#experience" className={active==='#experience'? 'active': ''} onClick={()=>setActive('#experience')}><BiBook /></a>
+      <a href="#services" className={active==='#services'? 'active': ''} onClick={()=>setActive('#services')}><RiServiceLine /></a>
+      <a href="#contact" className={active==='#contact'? 'active': ''} onClick={()=>setActive('#contact')}><BiMessageSquareDetail /></a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
