8520bffef0ea625fb1f6f6898f8b242dba84b77e
luck22y <luckykumar2204@gmail.com>
2023-05-02 17:57:57 +0530

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./about.css";
+import MeAbout from "../../assets/me-bg.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={MeAbout} alt="me" />
+          </div>
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FaAward className="about__icon" />
+                <h5>Experience</h5>
+                <small>8+ Months Working</small>
+              </article>
+              <article className="about__card">
+                <FiUsers className="about__icon" />
+                <h5>Clients</h5>
+                <small>Later</small>
+              </article>
+              <article className="about__card">
+                <VscFolderLibrary className="about__icon" />
+                <h5>Project</h5>
+                <small>Later</small>
+              </article>
+            </div>
+            <p>lorem ipsum</p>
 
-export default About
+            <a href="#contact">Let's Talk</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
