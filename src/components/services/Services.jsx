a3aecd2dbf6a952dfb460521d307a5aaaadd5664
luck22y <luckykumar2204@gmail.com>
2023-05-03 15:29:53 +0530

    service

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>Services</section >
-  )
-}
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__conatiner">
+        <article className="service">
+          <div className="service__head">
+            <h3>Fullstack Developer</h3>
+          </div>
+          <ul className="service__list">
+          <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF SERVICE 1 */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Frontend Developer</h3>
+          </div>
+          <ul className="service__list">
+          <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+          </ul>
+        </article>
+      {/* END OF SERVICE2 */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Backend Developer</h3>
+          </div>
+          <ul className="service__list">
+          <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
